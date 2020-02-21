bc4cd1cb15ebaffbdce5d10064e69da7c6cfae4f
Valadzko Kseniya <volodko.kj@gmail.com>
2020-02-21 15:23:39 +0300

    setting heroky

---
+++ github-bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == '__main__':
+    run_app(
+        name='PyCon-Bot-by-gloriasky',
+        version='1.0.0',
+        url='https://github.com/settings/apps/pycon-github-bot',
+    )
