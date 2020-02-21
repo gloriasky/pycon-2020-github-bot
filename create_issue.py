ac1bbb4a4c5d0c14e62c8edd6f4e50c3c12eaada
Valadzko Kseniya <volodko.kj@gmail.com>
2020-02-21 14:50:18 +0300

    initial commit

---
+++ create_issue.py
@@ -0,0 +1,19 @@
+import asyncio
+import os
+
+from aiohttp.client import ClientSession
+from octomachinery.github.api.tokens import GitHubOAuthToken
+from octomachinery.github.api.raw_client import RawGitHubAPI
+
+
+async def main():
+    access_token = GitHubOAuthToken(os.environ['GITHUB_TOKEN'])
+    async with ClientSession() as http_session:
+        gh = RawGitHubAPI(
+            access_token,
+            session=http_session,
+            user_agent='webknjaz',
+        )
+
+
+asyncio.run(main())
